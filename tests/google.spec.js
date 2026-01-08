da43f362b76a7b86dcc8d82cb782f08814424cff
mentor-prime <mentor.prime.education@gmail.com>
2026-01-07 22:35:58 -0600

    Update google.spec.js

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -4,3 +4,8 @@ test('Open Google', async ({ page }) => {
   await page.goto('https://www.google.com');
   await expect(page).toHaveTitle(/Google/);
 });
+
+test('Open Google and perform searching', async ({ page }) => {
+  await page.goto('https://www.google.com');
+  await expect(page).toHaveTitle(/Google/);
+});
